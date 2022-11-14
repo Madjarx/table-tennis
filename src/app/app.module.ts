efc32391676a60b789bfcdcc4b08ff6375332788
Madjarx <lukaradisavljevic30@gmail.com>
2022-11-14 16:33:24 +0100

    initiated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// import { MatchComponent } from './components/match/match.component';
+// import { PlayerListComponent } from './components/player-list/player-list.component';
+// import { MatchListComponent } from './components/match-list/match-list.component';
+// import { CreateMatchComponent } from './components/create-match/create-match.component';
+// import { CreatePlayerComponent } from './components/create-player/create-player.component';
+
+import {
+  PlayerComponent,
+  MatchComponent,
+  PlayerListComponent,
+  MatchListComponent,
+  CreatePlayerComponent,
+  CreateMatchComponent
+} from './components/index';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    MatchComponent,
+    PlayerListComponent,
+    MatchListComponent,
+    CreateMatchComponent,
+    CreatePlayerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/index.ts
@@ -0,0 +1,8 @@
+export { PlayerComponent as PlayerComponent } from "./player/player.component";
+export { MatchComponent as MatchComponent } from "./match/match.component";
+
+export { PlayerListComponent as PlayerListComponent } from "./player-list/player-list.component";
+export { MatchListComponent as MatchListComponent } from "./match-list/match-list.component";
+
+export { CreatePlayerComponent as CreatePlayerComponent } from "./create-player/create-player.component";
+export { CreateMatchComponent as CreateMatchComponent } from "./create-match/create-match.component";
